59b3acc2fac692a56bc3df2fff1fa196303f3cf9
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-12-29 23:34:22 +0900

    Add simple HTML dsl

---
+++ src/main/kotlin/io/monosketch/web/HtmlDsl.kt
@@ -0,0 +1,47 @@
+@file:Suppress("FunctionName")
+
+package io.monosketch.web
+
+import kotlinx.browser.document
+import kotlinx.dom.addClass
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLCollection
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.events.Event
+import org.w3c.dom.get
+
+fun Element.Div(classes: String, block: Element.() -> Unit): Element {
+    val node = document.createElement("div")
+    append(node)
+    node.addClass(classes)
+    node.block()
+    return node
+}
+
+val Number.px: String
+    get() = "${this}px"
+
+fun styleOf(vararg attributes: Pair<String, String>): String =
+    attributes.asSequence().map { "${it.first}: ${it.second}" }.joinToString(";")
+
+
+fun Element.setAttributes(vararg attrs: Pair<String, Any>) {
+    for ((key, value) in attrs) {
+        setAttribute(key, value.toString())
+    }
+}
+
+fun Element.style(vararg attrs: Pair<String, String>) = setAttributes("style" to styleOf(*attrs))
+
+fun HTMLCollection.forEach(block: (HTMLElement) -> Unit) {
+    for (i in 0 until length) {
+        val element = get(i)
+        if (element is HTMLElement) {
+            block(element)
+        }
+    }
+}
+
+fun Element.onClick(block: (Event) -> Unit) {
+    addEventListener("click", { block(it) })
+}
